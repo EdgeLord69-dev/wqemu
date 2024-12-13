5b0aa60a50b3d9ebd5b80c6af91237f5726f28c9
EdgeLord69-dev <66997533+EdgeLord69-dev@users.noreply.github.com>
2024-12-13 21:51:50 +0530

    Update su.sh

--- su.sh
+++ su.sh
@@ -4,9 +4,10 @@ echo "Keep support akuh.net thanks"
 echo "Please Wait"
 echo "===================================="
 wget -O akuh.zip https://media.githubusercontent.com/media/akuhnet/wqemu/master/akuh.zip
+wget https://cdimage.ubuntu.com/ubuntu-base/releases/24.04/release/ubuntu-base-24.04.1-base-amd64.tar.gz
 unzip akuh.zip
 unzip root.zip
-tar -xvf root.tar.xz
+tar -xvf ubuntu-base-24.04.1-base-amd64.tar.gz
 rm -rf akuh.zip root.zip root.tar.xz
 clear
 echo "===================================="
